5d5ebd6f98eb44f62f57924b96d162a36a1dcd1f
thomasltf628 <49809873+thomasltf628@users.noreply.github.com>
2025-04-02 23:29:08 +0800

    Update selenium_scrap_autotrader.py, added decriptive comment

--- backend/todo/Scrapping/selenium_scrap_autotrader.py
+++ backend/todo/Scrapping/selenium_scrap_autotrader.py
@@ -1,4 +1,4 @@
-# Goal of scraping: Getting the latest information of used car on sales and output to a csv file
+# Goal of scraping: Getting the latest information of used car on Autotrade platform sales and output to a csv file
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -39,7 +39,7 @@ def extract_car_info(car_info):
 
     return make, model, year
 
-# This website get location information by direct input of parameter in the url
+# This website could get location information by direct input of parameter in the url
 
 province = "on"
 city = "toronto"
@@ -68,25 +68,33 @@ data ={
         'link_to_buyer':[],
         'link_to_image':[],
     }
+
+# Create an empty dataframe
 df = pd.DataFrame(data)
+
 
 while (page_num <= page_to_scrap):
     try:
+        # Wait for the main element appear
         text_box = WebDriverWait(driver, timeout=10).until(
                 EC.presence_of_element_located((By.ID, "listingsWrapperMainListing"))
             )
         time.sleep(3)
         blocks = text_box.find_elements(By.CLASS_NAME, 'dealer-split-wrapper')
+
+        # Each block refer to one used car, scraping block by block
         for block in blocks:
             try:
                 element_position = block.location['y']
-                driver.execute_script(f"window.scrollTo(0, {element_position});")
+                driver.execute_script(f"window.scrollTo(0, {element_position});") # scrolling to next element
                 year_make_and_model =  block.find_element(By.CLASS_NAME, 'title-with-trim')
                 make, model, year = extract_car_info(year_make_and_model.text)
                 year = int(year)
                 print(make, model, year)
                 price = block.find_element(By.CLASS_NAME, 'price-amount')
                 price_todf = price.text
+
+                # Preprocessing of price scrapped
                 for char in '$,"':
                     price_todf = price_todf.replace(char, '')
                 print(price_todf)
@@ -94,8 +102,11 @@ while (page_num <= page_to_scrap):
                     price_todf = int(price_todf)
                 except:
                     continue
+                
                 mileage = block.find_element(By.CLASS_NAME, 'odometer-proximity')
                 mileage_todf = mileage.text
+
+                # Preprocessing of mileage scrapped
                 for char in 'KM, "':
                     mileage_todf = mileage_todf.replace(char, '')
                 print(mileage_todf)
@@ -117,22 +128,26 @@ while (page_num <= page_to_scrap):
                 
                 df.loc[len(df.index)] = [website, make, model, year, price_todf, mileage_todf, location.text,listing_date,link_to_buyer,link_to_image]
             except:
+                # Statistic data of failure
                 scrap_fail += 1
                 print (f'{scrap_fail} piece of information fails to be scrapped')
                 continue
             time.sleep(0.5)
         try:
+            # Navigate to next page by locating of the next page button
             next = driver.find_element(By.CLASS_NAME, f'page-link-{page_num}')
             page_num += 1
             print(page_num)
             time.sleep(3)
             next.click()
         except NoSuchElementException:
+            # Terminate the scraping if next page is not found
             print('404 not found')
             df.to_csv(f'{website}.csv', index=False)
             driver.quit()    
             break
     except Exception as e:
+        # Save the current data to csv and auit the Webdriver in case unhandled error happens
         print(f'Exception: {str(e)}')
         failures += 1
         if failures >= max_failures:
@@ -140,6 +155,7 @@ while (page_num <= page_to_scrap):
             print(f'Maximum failures ({max_failures}) reached. Exiting...')
             driver.quit()
             break
+# Save the current data to csv after the whole sucessful scrapping
 df.to_csv(f'{website}.csv', index=False)  
 
 
